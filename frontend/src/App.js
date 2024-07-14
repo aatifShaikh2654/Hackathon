8db2adcec666e6c2fb86b7de6ccbc46e41b7fb4c
aatif shaikh <aatifshaikh4564@gmail.com>
2024-07-14 09:05:46 +0530

    Aatif 7/13

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from './dashboard/Layout';
+import Dashboard from './dashboard/Dashboard';
+import Register from './components/Register'
+import Login from './components/Login';
 
 function App() {
-  return (  
-        <h1 style={{textAlign: "center"}}>Hello Hackathon!</h1>
+
+  // const { getUser } = useContext(UserContext)
+  // const token = localStorage.getItem("token")
+
+  // useEffect(() => {
+  //   if (token) {
+  //     getUser();
+  //   }
+  // }, [token])
+
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Register />} />
+          <Route path='/login' element={<Login/>} />
+          <Route exact path="/dashboard" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
+// Test
 export default App;
