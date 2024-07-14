770795741ce6519397b6a4ee333933a034f25147
Uvesh-khan11 <uveshpathan665@gmail.com>
2024-07-14 08:29:30 +0530

    uvesh 8:29

--- backend/mainapp/admin.py
+++ backend/mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Book
 # Register your models here.
+@admin.register(Book)
+class ShowBook(admin.ModelAdmin):
+    list_display = ['title', 'author']
--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    isbn = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    year = models.DateField()
+    genre = models.CharField(max_length=100)
+    quantity = models.CharField(max_length=100)
+    available = models.BooleanField(default=True)
