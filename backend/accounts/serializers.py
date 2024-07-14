4fa3902a2641d0bb612d046170891e0b49299fcf
ishaqshaikhh <ishaqshaikh401@gmail.com>
2024-07-14 12:18:47 +0530

    ishaq:11

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -6,4 +6,4 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = "__all__"
+        exclude = ['password']
